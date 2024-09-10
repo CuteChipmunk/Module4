30d398446159581c4e6dd3aa470f0dadaf8d4d49
CuteChipmunk <katerina131299@gmail.com>
2024-09-10 18:21:06 +0300

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,13 @@
+from fake_math import divide as dv1
+from true_math import divide as dv2
+print(dv1(2, 0))
+print(dv2(2,0))
+
+result1 = dv1(69, 3)
+result2 = dv1(3, 0)
+result3 = dv2(49, 7)
+result4 = dv2(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
